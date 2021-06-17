2b7f023fc583548f5f998c0fa2f9196fa5c0812a
Bryan LEGRAS <blegras@0801209e.local>
2021-06-17 16:26:38 +0200

    Ajout de l'entité Tasks

---
+++ src/Entity/Tasks.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TasksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TasksRepository::class)
+ */
+class Tasks
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $creationDate;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $completionDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getCompletionDate(): ?\DateTimeInterface
+    {
+        return $this->completionDate;
+    }
+
+    public function setCompletionDate(?\DateTimeInterface $completionDate): self
+    {
+        $this->completionDate = $completionDate;
+
+        return $this;
+    }
+}
