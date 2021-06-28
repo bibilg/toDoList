09c6131615b4a07742911c3ad2f389802af82619
Bryan LEGRAS <blegras@0801209e.local>
2021-06-28 11:14:34 +0200

    Creation dun formulaire pour les tasks sur la page principale

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Tasks;
+use App\Form\TasksType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,12 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', []);
+        $task = new Tasks();
+        
+        $form = $this->createForm(TasksType::class, $task); // Cette manière si on a utilisé une classe Type dédié. Sinon on crée une Tasks puis createFormBuilder->add()....->getForm()
+
+        return $this->render('default/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
