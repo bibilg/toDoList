6149f7948c863556bb3f0c0b666b6715c6bb2e4c
Bryan LEGRAS <blegras@0801209e.local>
2021-06-28 11:48:14 +0200

    Ajout en BDD de la task après validation du formulaire

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Tasks;
 use App\Form\TasksType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +17,23 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
         $task = new Tasks();
         
         $form = $this->createForm(TasksType::class, $task); // Cette manière si on a utilisé une classe Type dédié. Sinon on crée une Tasks puis createFormBuilder->add()....->getForm()
+
+        $form->handleRequest($request); // Permets de traiter les données du formulaire 
+
+        if($form->isSubmitted() && $form->isValid()){ // Si le formulaire est envoyé et valide 
+
+            $task->setCreationDate(new \DateTime());
+
+            $manager->persist($task);
+            $manager->flush();
+
+            return $this->redirectToRoute('home');
+        }
 
         return $this->render('default/index.html.twig', [
             'form' => $form->createView(),
