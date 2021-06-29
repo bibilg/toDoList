4467920fb6d94c059c3c382c956c34c2da8336b9
Bryan LEGRAS <blegras@0801209e.local>
2021-06-29 15:54:32 +0200

    Ajout d'une animation JS quand on clique sur valider ou supprimer une page sur l'index

---
+++ public/assets/js/main.js
@@ -0,0 +1,10 @@
+const validate = document.querySelector('#validate');
+const remove = document.querySelector('#remove');
+
+validate.addEventListener('click', (event) => {
+    validate.style.background = 'black';
+})
+
+remove.addEventListener('click', (event) => {
+    remove.style.background = 'black';
+})
